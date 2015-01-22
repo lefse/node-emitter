6c4a411a87140eac63f32c01e5f6df2a82b8cd79
Brent Larson <lefse@revolverdesignlab.com>
2015-01-21 22:34:11 -0800

    Added MQTT and relay

--- app.js
+++ app.js
@@ -9,41 +9,54 @@ var default_payload = {
   }
 };
 
+var polling_period = 5000;
+
 var in_wifi = require('./lib/in/wifi/wifi_sensor'),
     in_flow = require('./lib/in/flow-sensor/YF-S201.js'),
     in_gps = require('./lib/in/gps/nmea-0183.js'),
     in_temp = require('./lib/in/temp/DS18B20.js'),
-    in_mqtt = require('./lib/inout/mqtt/skawtus_mqtt'),    
-    out_http = require('./lib/inout/http/skawtus_http'),
-    out_mqtt = require('./lib/inout/mqtt/skawtus_mqtt'),
-    out_local_json = require('./lib/out/log/local_json');
-    //out_relay = require('./lib/in/gpio/gpio_relay.js'),    
+    inout_mqtt = require('./lib/inout/mqtt/skawtus_mqtt'),    
+    inout_http = require('./lib/inout/http/skawtus_http'),
+    out_local_json = require('./lib/out/log/local_json'),
+    out_relay = require('./lib/out/relay/gpio_relay.js');    
 
 var publisher  = pubsub.createPublisher(),
     sub_wifi = pubsub.createSubscriber(),
     sub_temp = pubsub.createSubscriber(),
     sub_flow = pubsub.createSubscriber(),
     sub_gps = pubsub.createSubscriber();
-
-var polling_period = 5000;
+    sub_mqtt = pubsub.createSubscriber();
 
 sub_wifi.subscribe('wifi');
 sub_flow.subscribe('flow-sensor');
 sub_gps.subscribe('gps');
 sub_temp.subscribe('temp');
+sub_mqtt.subscribe('mqtt');
 
 // Message events from each subscriber channel.
 sub_wifi.on('message', function(channel, message) {
-  console.log("Do something with :" + channel, message);
+  //console.log("Do something with :" + channel, message);
 });
 sub_temp.on('message', function(channel, message) {
-  console.log("Do something with :" + channel, message);
+  //console.log("Do something with :" + channel, message);
 });
 sub_flow.on('message', function(channel, message) {
-  console.log("Do something with :" + channel, message);
+  //console.log("Do something with :" + channel, message);
 });
 sub_gps.on('message', function(channel, message) {
-  console.log("Do something with :" + channel, message);
+  //console.log("Do something with :" + channel, message);
+});
+sub_mqtt.on('message', function(channel, message) {
+    if(message.topic == "skawtus/usr/devices/relay/value/set") {
+        console.log(message.msg)
+        if(message.msg == "true")   {
+            console.log("Relay ON");
+            out_relay.on();
+        } else {
+            console.log("Relay OFF");
+            out_relay.off();
+        }
+    }
 });
 
 // Check data from each sensor and publish to the channel.
@@ -53,6 +66,7 @@ function checkState(data){
   in_flow.check(data, publisher, function(data, publisher) {});
   in_gps.check(data, publisher, function(data, publisher) {});
   in_temp.check(data, publisher, function(data, publisher) {});
+  inout_mqtt.publish(data, publisher);
 
   setTimeout(checkState, polling_period,{});
 }
